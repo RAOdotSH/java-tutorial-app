a44d8db68c64f10b0afd1538656a936d6f090012
DP <daulatrao24@gmail.com>
2024-09-16 14:27:27 +0530

    Update: Repo

--- src/main/java/com/rao/tutorial/repository/TutorialRepository.java
+++ src/main/java/com/rao/tutorial/repository/TutorialRepository.java
@@ -2,7 +2,7 @@ package com.rao.tutorial.repository;
 
 import java.util.List;
 
-import org.springgramework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.rao.tutorial.model.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
