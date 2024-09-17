7d5e47364ecbdfbdc10eada91f6a3eca028d6e78
DP <daulatrao24@gmail.com>
2024-09-17 12:46:58 +0530

    Add: Logger

--- src/main/java/com/rao/tutorial/TutorialApplication.java
+++ src/main/java/com/rao/tutorial/TutorialApplication.java
@@ -2,12 +2,17 @@ package com.rao.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class TutorialApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(TutorialApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
+		log.info("Sever started");
 	}
 
 }
